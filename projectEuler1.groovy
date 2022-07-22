ed7418cc0969a6351f825a66322e913e5d7a8339
ghector6 <ghectormus@gmail.com>
2022-07-21 21:30:27 -0500

    Testing range method in for in param

--- projectEuler1.groovy
+++ projectEuler1.groovy
@@ -1,10 +1,8 @@
 Long totalSum = 0
-def range = (0..10)
-for(num in range){
+//def range = (0..10)
+for(num in 0..<10){
     if(num % 3 == 0 || num % 5 == 0){
         totalSum += num 
-    }
-    
+    }    
 }
-
 println totalSum
