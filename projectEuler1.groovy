22952a911ee3499462734f050e05cae4042838ad
ghector6 <ghectormus@gmail.com>
2022-07-21 21:35:13 -0500

    Commenting code for pedagogic porpouses

--- projectEuler1.groovy
+++ projectEuler1.groovy
@@ -1,6 +1,6 @@
-Long totalSum = 0
-for(num in 0..<10){
-    if(num % 3 == 0 || num % 5 == 0){
+Long totalSum = 0 //Defining a Long type variable to take summatory of numbers 
+for(num in 0..<10){ //Looping using the range method 
+    if(num % 3 == 0 || num % 5 == 0){ //Conditional for numbers that are adding 
         totalSum += num 
     }    
 }
