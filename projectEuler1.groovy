83ec10f6455d6eee44e1a0ab14debefb8755aa85
ghector6 <ghectormus@gmail.com>
2022-08-04 07:45:55 -0500

    Finished p1, refactor awaiting

--- projectEuler1.groovy
+++ projectEuler1.groovy
@@ -1,5 +1,5 @@
 Long totalSum = 0 //Defining a Long type variable to take summatory of numbers 
-for(num in 0..<10){ //Looping using the range method 
+for(num in 0..<1000){ //Looping using the range method 
     if(num % 3 == 0 || num % 5 == 0){ //Conditional for numbers that are adding 
         totalSum += num 
     }    
