5b7d7672cf143bc8eb3194ffc23994a80fa95a24
ghector6 <ghectormus@gmail.com>
2022-08-10 09:23:37 -0500

    Step 5: Adding join to convert array to string

--- projectEuler4.groovy
+++ projectEuler4.groovy
@@ -2,14 +2,15 @@ class Palindrome{
      
 
     static multiplr(range){
-        List multList = []
+        def multList = []
         for(def i = 1; i <= range; i++){
             for(def f = 1; f <= range; f++){
                 multList << i * f
             }                        
         }
-        println multList.max()
+        println multList.join(", ").toString()        
     }
+    
 }
 
-Palindrome.multiplr(99)
+Palindrome.multiplr(10)
