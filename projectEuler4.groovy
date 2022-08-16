7db33e10a0d84d751c84a1d2522a5b66f7c23fa1
ghector6 <ghectormus@gmail.com>
2022-08-16 16:54:07 -0500

    Adding a as Integer method to convert again to string and printing the arr that took the list of numbers that were pushed to  the List

--- projectEuler4.groovy
+++ projectEuler4.groovy
@@ -1,17 +1,22 @@
 class Palindrome {
     static multplyr(range){
-        def res = 0
+        Integer res = 0
         List arr = []
 
         for(def i = 1; i < range; i++){
             for(def j = 1; j < range; j++){
                 res = i*j
                 def numString = res.toString().reverse()
-                println numString.class.name
+                if(numString as Integer == res){
+                    def parsedNumStr = numString as Integer
+                    arr << parsedNumStr
+                    
+                }
                 
             }
 
         }
+        println arr
         
     }
 }
